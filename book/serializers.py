37f9e2ebe2bb8fda1da109627dccd3ef4a1c0435
temirovuz <temirov-muhammad@mail.ru>
2025-01-31 17:29:49 +0500

    add permission and create, list books

--- book/serializers.py
+++ book/serializers.py
@@ -3,6 +3,7 @@ from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import SerializerMethodField
 
 from book.models import Book, Author, Genre, Language
+from book.permissions import IsAdmin
 
 
 class CreateAuthorSerializer(serializers.ModelSerializer):
@@ -24,10 +25,21 @@ class CreateLanguageSerializer(serializers.ModelSerializer):
 
 
 class CreateBookSerializer(serializers.ModelSerializer):
-    authors = CreateAuthorSerializer(many=True)
-    genres = CreateGenreSerializer(many=True)
+    authors = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), many=True)
+    genres = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), many=True)
+    language = serializers.PrimaryKeyRelatedField(queryset=Language.objects.all())
     class Meta:
         model = Book
         fields = ['name', 'isbn', 'summary', 'publication_date', 'authors', 'genres', 'language', 'daily_price',
                   'available_copies']
 
+
+
+class ListBooksSerializer(serializers.ModelSerializer):
+    authors = CreateAuthorSerializer(many=True)
+    genres = CreateGenreSerializer(many=True)
+
+    class Meta:
+        model = Book
+        fields = ['name', 'isbn', 'summary', 'publication_date', 'authors', 'genres', 'language', 'daily_price',
+                  'available_copies']
--- book/urls.py
+++ book/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from book.views import BookApiView
+from book.views import BookApiView, BookCreateAPIView
 
 app_name = 'book'
 
 urlpatterns = [
-    path('book/', BookApiView.as_view(), name='book-list'),
+    path('books/', BookApiView.as_view(), name='book-list'),
+    path('create/', BookCreateAPIView.as_view(), name='book-create'),
 ]
